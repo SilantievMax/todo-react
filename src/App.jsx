c3b3db722ea2b2b4852d8f3263f9bcebbac110c0
SilantievMax <nohrin.max@yandex.ru>
2022-09-21 20:20:27 +0300

    031 Коллекции и ключи2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import TodoList from "./components/TodoList";
 const App = () => {
   const todoData = [
     { id: 1, label: "Drink Coffee", important: false },
-    { id: 2, label: "Drink Coffee", important: true },
+    { id: 2, label: "Make Awesome App", important: true },
     { id: 3, label: "Have a lunch", important: false },
   ];
 
