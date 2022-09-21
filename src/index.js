12553d69dbf9e30f9560332bc3eefdd05ba05653
SilantievMax <nohrin.max@yandex.ru>
2022-09-21 17:58:09 +0300

    026 JSX

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const TodoList = () => {
+  const items = ["Lesrn React", "Build Awesome App"];
   return (
     <ul>
-      <li>Lesrn React</li>
-      <li>Build Awesome App</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
   );
 };
 
 const AppHeader = () => {
-  return <h1>Mt Todo List</h1>;
+  return <h1>My Todo List</h1>;
 };
 
 const SearchPanel = () => {
