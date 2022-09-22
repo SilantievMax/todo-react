db2beab26443a33fdc464c1ce737abda47b38001
SilantievMax <nohrin.max@yandex.ru>
2022-09-22 14:21:54 +0300

    034 Компоненты-классы

--- src/components/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/components/ItemStatusFilter/ItemStatusFilter.jsx
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ItemStatusFilter = () => {
-  return (
-    <div className="mt-2 h-10 w-80">
-      <button
-        className="w-1/3 border border-cyan-600 bg-cyan-600 py-0.5 hover:bg-cyan-700 h-full"
-        type="button"
-      >
-        All
-      </button>
-      <button
-        className="w-1/3 border border-cyan-600 py-0.5 hover:bg-cyan-700 h-full"
-        type="button"
-      >
-        Active
-      </button>
-      <button
-        className="w-1/3 border border-cyan-600 py-0.5 hover:bg-cyan-700 h-full rounded-r"
-        type="button"
-      >
-        Dones
-      </button>
-    </div>
-  );
-};
-
-export default ItemStatusFilter;
+export default class ItemStatusFilter extends Component {
+  render() {
+    return (
+      <div className="mt-2 h-10 w-80">
+        <button
+          className="w-1/3 border border-cyan-600 bg-cyan-600 py-0.5 hover:bg-cyan-700 h-full"
+          type="button"
+        >
+          All
+        </button>
+        <button
+          className="w-1/3 border border-cyan-600 py-0.5 hover:bg-cyan-700 h-full"
+          type="button"
+        >
+          Active
+        </button>
+        <button
+          className="w-1/3 border border-cyan-600 py-0.5 hover:bg-cyan-700 h-full rounded-r"
+          type="button"
+        >
+          Dones
+        </button>
+      </div>
+    );
+  }
+}
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,53 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 
-const TodoListItem = ({ label, important = false }) => {
-  return (
-    <span className="px-3 flex justify-between content-center border border-cyan-600 my-2 rounded w-full block py-2 px-1">
-      <span className={important ? "text-cyan-400" : "text-slate-200"}>
-        {label}
+export default class TodoListItem extends Component {
+  render() {
+    return (
+      <span className="px-3 flex justify-between content-center border border-cyan-600 my-2 rounded w-full block py-2 px-1">
+        <span className={this.important ? "text-cyan-400" : "text-slate-200"}>
+          {this.label}
+        </span>
+        <div>
+          <button
+            className="border-2 border-rose-600 rounded p-1 text-rose-600"
+            type="button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+          </button>
+          <button
+            className="border-2 border-lime-600 rounded p-1 text-lime-600 ml-2"
+            type="button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+          </button>
+        </div>
       </span>
-      <div>
-        <button
-          className="border-2 border-rose-600 rounded p-1 text-rose-600"
-          type="button"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-            />
-          </svg>
-        </button>
-        <button
-          className="border-2 border-lime-600 rounded p-1 text-lime-600 ml-2"
-          type="button"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-            />
-          </svg>
-        </button>
-      </div>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
