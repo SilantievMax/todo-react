7b986284a04a4b203bd71b4c8fc6cae878a6c7d3
SilantievMax <nohrin.max@yandex.ru>
2022-09-23 13:15:19 +0300

    040 setState() - удаление элемента

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import AppHeader from "./components/AppHeader";
 import ItemStatusFilter from "./components/ItemStatusFilter";
 import SearchPanel from "./components/SearchPanel";
 import TodoList from "./components/TodoList";
 
-const App = () => {
-  const todoData = [
-    { id: 1, label: "Drink Coffee", important: false },
-    { id: 2, label: "Make Awesome App", important: true },
-    { id: 3, label: "Have a lunch", important: false },
-  ];
+export default class App extends Component {
+  state = {
+    todoData: [
+      { id: 1, label: "Drink Coffee", important: false },
+      { id: 2, label: "Make Awesome App", important: true },
+      { id: 3, label: "Have a lunch", important: false },
+    ],
+  };
 
-  return (
-    <div className="w-1/2 m-auto mt-5 bg-slate-700 p-3 rounded font-sans text-slate-200">
-      <AppHeader />
-      <div className="flex justify-between">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const inx = todoData.findIndex((el) => el.id === id);
+
+      const newArray = [...todoData.slice(0, inx), ...todoData.slice(inx + 1)];
+
+      return {
+        todoData: newArray,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="w-1/2 m-auto mt-5 bg-slate-700 p-3 rounded font-sans text-slate-200">
+        <AppHeader />
+        <div className="flex justify-between">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
       </div>
-      <TodoList todos={todoData} onDeleted={(id) => console.log(id)} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
